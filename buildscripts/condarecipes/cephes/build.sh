fa48686dec969d84ffabf95d275834af91e1a260
Juan Luis Cano Rodríguez <juanlu001@gmail.com>
2016-03-06 15:38:49 +0100

    Fix build.sh for OS X

--- buildscripts/condarecipes/cephes/build.sh
+++ buildscripts/condarecipes/cephes/build.sh
@@ -4,4 +4,8 @@ export LIBRARY_PATH=$PREFIX/lib
 
 make CFLAGS='-O2 -Wall -fno-builtin -fPIC'
 
-install -Dm755 libmd.a $LIBRARY_PATH/libmd.a
+if [ "$(uname)" == "Linux" ]; then
+  install -Dm755 libmd.a $LIBRARY_PATH/libmd.a
+elif [ "$(uname)" == "Darwin" ]; then
+  install -m755 libmd.a $LIBRARY_PATH/libmd.a
+fi
